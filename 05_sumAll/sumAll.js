480c26a0441cf3991fc1ce7d55e9c2a90fc586d0
Lucas Rosén <49569378+LucasRosen@users.noreply.github.com>
2024-04-20 16:40:14 +0200

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0) return "ERROR";
+    if (isNaN(min) || isNaN(max)) return "ERROR";
+    if (typeof(min) == "string" || typeof(max) == "string") return "ERROR";
+    // if min is larger then max, reverse them
+    if (min > max) {
+        let temp = max;
+        max = min;
+        min = temp;
+    }
 
+    let totalSum = 0;
+    for (let i = min; i <= max; i++) {
+        totalSum += i;
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line

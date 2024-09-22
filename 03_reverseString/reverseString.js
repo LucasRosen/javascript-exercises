675e156bdd3f5ef651a9ab0f87641dd24d2ca396
Lucas Rosén <49569378+LucasRosen@users.noreply.github.com>
2024-09-22 21:16:44 +0200

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(s) {
+    let reversedArray = s.split("").reverse();
+    let reversedString = reversedArray.join("");
 
+    return reversedString;
 };
 
 // Do not edit below this line

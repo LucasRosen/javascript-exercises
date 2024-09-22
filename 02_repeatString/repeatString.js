07ef612aa8133a16dc29168bbaf29fd643ef640a
Lucas Rosén <49569378+LucasRosen@users.noreply.github.com>
2024-09-22 20:23:32 +0200

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(s, n) {
+    if (n < 0) {return "ERROR"} else if (n === 0) {return ""};
 
+    let newS = "";
+    for (let i = 0; i < n; i++) {
+        newS += s;
+    }
+
+    return newS;
 };
 
 // Do not edit below this line

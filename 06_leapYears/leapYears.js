58d04f052ca8cc778d920ae1b648257e14f8ea87
Lucas Rosén <49569378+LucasRosen@users.noreply.github.com>
2024-09-22 22:10:47 +0200

    Complete exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // year % 400 === 0 --LEAP YEAR
+    // year % 100 === 0 --NOT LEAP YEAR
+    // year % 4 === 0 --LEAP YEAR
 
+    if (year % 400 === 0) {return true}
+    if (year % 4 === 0 && year % 100 !== 0) {return true}
+    return false
 };
 
 // Do not edit below this line

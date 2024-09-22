f7054f0e35e05b66a664bf8601c233404ee5a46d
Lucas Rosén <49569378+LucasRosen@users.noreply.github.com>
2024-09-22 21:48:53 +0200

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        console.log(arr[i])
+        for (let j = 0; j < valuesToRemove.length; j++) {
+            if (valuesToRemove[j] === arr[i]) {
+                arr.splice(i, 1);
+                console.log(arr)
+            }
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line

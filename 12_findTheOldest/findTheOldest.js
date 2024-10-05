987704f43e5b55f6c1fceefbc70f1030455ebd62
Lucas Rosén <49569378+LucasRosen@users.noreply.github.com>
2024-10-05 17:30:16 +0200

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const sortedByAge = people.sort((a, b) => {
+        if (!a.yearOfDeath) {a.yearOfDeath = 2024};
+        if (!b.yearOfDeath) {b.yearOfDeath = 2024};
 
+        const lastPerson = a.yearOfBirth - a.yearOfDeath;
+        const nextPerson = b.yearOfBirth - b.yearOfDeath;
+
+        return lastPerson > nextPerson ? 1 : -1;
+    })
+
+    return sortedByAge[0];
 };
 
 // Do not edit below this line

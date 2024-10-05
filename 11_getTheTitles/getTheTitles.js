66962d48a6a00f5218d83e560f294300877d5f8e
Lucas Rosén <49569378+LucasRosen@users.noreply.github.com>
2024-10-05 16:19:14 +0200

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    const titles = books.reduce((arr, book) => {
+        arr.push(book.title);
+        return arr;
+    }, [])
 
+    return titles;
 };
 
 // Do not edit below this line

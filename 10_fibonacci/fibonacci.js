695bb44670bf12380ecf031c8b6ae76f5ca33a14
Lucas Rosén <49569378+LucasRosen@users.noreply.github.com>
2024-10-05 15:59:48 +0200

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {return "OOPS"};
+    if (n == 0) {return 0};
 
+    let prev1 = 1;
+    let prev2 = 0;
+    for (let i = 2; i <= n; i++) {
+        let cur = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = cur;
+    }
+
+    return prev1;
 };
 
 // Do not edit below this line

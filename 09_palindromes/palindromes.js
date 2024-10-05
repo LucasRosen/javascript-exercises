f7e5d7bebe5e37f67cb1aca8c01b8f074b94bdd0
Lucas Rosén <49569378+LucasRosen@users.noreply.github.com>
2024-10-05 15:46:23 +0200

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase().split("");
+    // char == "!" || char == "." || char == ","
+    for (let i = str.length; i >= 0; i--) {
+        if (str[i] == "!" || str[i] == "." || str[i] == "," || str[i] == " ") {
+            str.splice(i, 1);
+        }
+    }
 
+    const reversedStr = str.toReversed().join("");
+    str = str.join("");
+    console.log(str)
+    console.log(reversedStr)
+
+    return str === reversedStr;
 };
 
 // Do not edit below this line
